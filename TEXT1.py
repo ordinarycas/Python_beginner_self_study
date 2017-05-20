6496e6ed3ce900f35b435478a32489f52644e2b4
xcqvmywoj <10210215@mail.chihlee.edu.tw>
2017-05-21 00:38:00 +0800

    0521

--- TEXT1.py
+++ TEXT1.py
@@ -4,16 +4,21 @@ Python3 默認就是用 utf-8
 '''
 
 #-*- coding: utf-8 -*-
-
-import getpass
+#while回圈
 
 age_of_ironman = 35
+count = 0
 
-guess_age = int(input("guess age:"))
-
-if guess_age == age_of_ironman:
-    print("Bingo, You got it!!!")
-elif guess_age > age_of_ironman:
-    print("You may think smaller...")
+while count < 3:
+    guess_age = int(input("guess age:"))
+    if guess_age == age_of_ironman:
+        print("Bingo, You got it!!!")
+        break
+    elif guess_age > age_of_ironman:
+        print("You may think smaller...")
+    else:
+        print("You may think bigger...")
+    count +=1
 else:
-    print("You may think bigger...") 
+    #當count不為真會執行下面這行
+    print("You have tried too many, exit")
